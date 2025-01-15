3dba8b350fed066e04fea289de8d027810fc2b14
kakamatong <631603920@qq.com>
2025-01-15 22:52:37 +0800

    优化结构

--- game/main.lua
+++ game/main.lua
@@ -4,7 +4,7 @@ local sprotoloader = require "sprotoloader"
 local max_client = 64
 
 skynet.start(function()
-	skynet.error("Server start")
+	LOG.info("Server start")
 	skynet.uniqueservice("protoloader")
 	if not skynet.getenv "daemon" then
 		local console = skynet.newservice("console")
@@ -17,6 +17,6 @@ skynet.start(function()
 		maxclient = max_client,
 		nodelay = true,
 	})
-	skynet.error("Watchdog listen on " .. addr .. ":" .. port)
+	LOG.info("Watchdog listen on " .. addr .. ":" .. port)
 	skynet.exit()
 end)
--- game/protoloader.lua
+++ game/protoloader.lua
@@ -1,13 +1,9 @@
--- module proto as examples/proto.lua
-package.path = "./examples/?.lua;" .. package.path
-
 local skynet = require "skynet"
-local sprotoparser = require "sprotoparser"
 local sprotoloader = require "sprotoloader"
-local proto = require "proto"
 
 skynet.start(function()
-	sprotoloader.save(proto.c2s, 1)
-	sprotoloader.save(proto.s2c, 2)
+	sprotoloader.register("proto/c2s.sproto", 1)
+	sprotoloader.register("proto/s2c.sproto", 2)
 	-- don't call skynet.exit() , because sproto.core may unload and the global slot become invalid
 end)
+
