3db414cf9f8f33d242c11b8f5950d826b40ee58a
秋刀鱼 <matongkaka@163.com>
2025-05-22 09:12:52 +0000

    优化匹配服务

--- game/services/match.lua
+++ game/services/match.lua
@@ -7,17 +7,25 @@ local queueNum = 4
 local queueUserids = {}
 local dTime = 1
 
+-- 匹配成功后通知agent
+local function reportToAgent(userid,gamedata)
+    local user = users[userid]
+    local agent = user.agent
+
+    skynet.call(agent, "lua", "enterGame", userid2,gamedata)
+end
+
+-- 匹配成功
 local function matchSuccess(userid1, userid2)
     -- 1.创建游戏
     -- 2.删除queue里的用户
     -- 3.通知agent
-    local user1 = users[userid1]
-    local user2 = users[userid2]
-    local agent1 = user1.agent
-    local agent2 = user2.agent
-    skynet.call(agent1, "lua", "enterGame", userid2)
+    
+    reportToAgent(userid1, {})
+    reportToAgent(userid2, {})
 end
 
+-- 检查队列
 local function checkQueue(queueid)
     local que = queueUserids[queueid]
     -- 循环前一个跟后一个比较rate，差值小于0.05，匹配成功
@@ -29,13 +37,34 @@ local function checkQueue(queueid)
             local user2 = users[userid2]
             if math.abs(user1.rate - user2.rate) < 0.05 then
                 LOG.info("match success %d %d", userid1, userid2)
-                
+                matchSuccess(userid1, userid2)
             end
         end
     end
-        
 end
 
+-- 离开队列
+local function leaveQueue(userid)
+    LOG.info("leaveQueue %d", userid)
+    if not users[userid] then
+        return false
+    end
+    local queueid = users[userid].queueid
+    if not queueUserids[queueid] then
+        return false
+    end 
+    for i, v in ipairs(queueUserids[queueid]) do
+        if v == userid then
+            table.remove(queueUserids[queueid], i)
+            break
+        end
+    end
+    users[userid] = nil
+    LOG.info("queueUserids %s", UTILS.tableToString(queueUserids))
+    return true
+end
+
+-- 开始匹配
 function CMD.start()
     LOG.info("match start")
 
@@ -52,10 +81,12 @@ function CMD.start()
 	end)
 end
 
+-- 停止匹配
 function CMD.stop()
     LOG.info("match stop")
 end
 
+-- 进入队列
 function CMD.enterQueue(agent, userid, queueid, rate)
     LOG.info("enterQueue %d", userid)
     if not users[userid] then
@@ -79,26 +110,11 @@ function CMD.enterQueue(agent, userid, queueid, rate)
         end
     end
     table.insert(queueUserids[queueid], index, userid)
-    return true
 end
 
+-- 离开队列
 function CMD.leaveQueue(userid)
-    LOG.info("leaveQueue %d", userid)
-    if not users[userid] then
-        return false
-    end
-    local queueid = users[userid].queueid
-    if not queueUserids[queueid] then
-        return false
-    end 
-    for i, v in ipairs(queueUserids[queueid]) do
-        if v == userid then
-            table.remove(queueUserids[queueid], i)
-            break
-        end
-    end
-    LOG.info("queueUserids %s", UTILS.tableToString(queueUserids))
-    return true
+    leaveQueue(userid)
 end
 
 skynet.start(function()
