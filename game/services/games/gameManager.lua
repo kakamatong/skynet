f930f5dfbc3f8127ffdc3a341853c14ed5486e2c
秋刀鱼 <matongkaka@163.com>
2025-05-28 06:37:28 +0000

    游戏模块

--- game/services/games/gameManager.lua
+++ game/services/games/gameManager.lua
@@ -3,6 +3,35 @@ require "skynet.manager"
 
 local CMD = {}
 local name = "gameManager"
+local allGames = {}
+local roomid = 0
+
+-- 创建游戏
+function CMD.createGame(gameid, players, gameData)
+    roomid = roomid + 1
+    local name = "game" .. gameid
+    local game = skynet.newservice(name)
+    skynet.call(game, "lua", "start", {gameid = gameid, players = players, gameData = gameData, roomid = roomid})
+    if not allGames[gameid] then
+        allGames[gameid] = {}
+    end
+    allGames[gameid][roomid] = game
+    
+    return roomid
+end
+
+-- 销毁游戏
+function CMD.destroyGame(gameid, roomid)
+    local game = allGames[gameid][roomid]
+    skynet.call(game, "lua", "stop")
+    allGames[gameid][roomid] = nil
+    return true
+end
+
+-- 获取游戏
+function CMD.getGame(gameid, roomid)
+    return allGames[gameid][roomid]
+end
 
 skynet.start(function()
     skynet.dispatch("lua", function(session, source, cmd, ...)
